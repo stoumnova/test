232e8a3e518219119262a9db2d3ab533b92b8916
stoumnova <stoumnova@inbox.ru>
2025-05-31 19:58:47 +0300

    Update prelod666.js

--- prelod666.js
+++ prelod666.js
@@ -10,7 +10,7 @@ overlay.style.cssText = `
     left: 0;
     width: 100%;
     height: 100%;
-    background-color: rgba(255, 255, 255, 1);
+    background-color: rgba(255, 255, 255, 0.7);
     z-index: 999;
 `;
 
