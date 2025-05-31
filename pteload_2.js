faa380c7ec348087116c37295f4a5c73be384b5f
stoumnova <stoumnova@inbox.ru>
2025-05-31 17:17:19 +0300

    Create pteload_2.js

---
+++ pteload_2.js
@@ -0,0 +1,17 @@
+<style>
+        .centr {
+            display: flex; 
+            justify-content: center; 
+            align-items: center; 
+            height: 100vh; 
+            background-color: #f1f1f1;
+            padding: -100px;
+        }
+        .content {
+        background-color: #cef0f1;
+    }
+  </style>
+<div class="centr">
+<script src="https://unpkg.com/@dotlottie/player-component@2.7.12/dist/dotlottie-player.mjs" type="module"></script> 
+<dotlottie-player src="https://lottie.host/04569e61-a596-4277-ba5c-b42401785d46/WHhINCew1V.lottie" background="transparent" speed="1" style="width: 300px; height: 300px;" loop autoplay></dotlottie-player>
+</div>
